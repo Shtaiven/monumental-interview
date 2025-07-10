eede47a74f78c00d191cb4e75713b2b5654f4f8b
Steven Eisinger <seisinger@smart-robotics.nl>
2025-07-10 17:33:21 +0200

    fix: works with pixi

--- build.sh
+++ build.sh
@@ -3,5 +3,10 @@
 
 mkdir build >/dev/null 2>&1
 cd build
-cmake ..
+if [[ -n "$CONDA_DEFAULT_ENV" ]]; then
+    echo "Using conda env to build"
+    cmake -DCMAKE_C_COMPILER=x86_64-conda-linux-gnu-gcc -DCMAKE_CXX_COMPILER=x86_64-conda-linux-gnu-g++ ..
+else
+    cmake ..
+fi
 make
--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 EXECUTABLE_PATH="build/robot-client"
 
 if [ -f ${EXECUTABLE_PATH} ]; then
-    ./${EXECUTABLE_PATH}
+    ./${EXECUTABLE_PATH} "ws://91.99.103.188:8765"
 else
     echo -e "${EXECUTABLE_PATH} doesn't exist. Try building first with\n\n./build.sh"
 fi
