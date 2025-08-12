81da9b354df2dc6c4cd7ff1fe11fc68c1d16fff4
Steven Eisinger <steven.a.eisinger@gmail.com>
2025-08-12 21:44:18 +0200

    add clang tidy, small cleanup

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 # Build the project
 
 mkdir build >/dev/null 2>&1
-cd build
+cd build || exit
 if [[ -n "$CONDA_DEFAULT_ENV" ]]; then
     echo "Using conda env to build"
-    cmake -DCMAKE_C_COMPILER=x86_64-conda-linux-gnu-gcc -DCMAKE_CXX_COMPILER=x86_64-conda-linux-gnu-g++ ..
+    cmake -DCMAKE_C_COMPILER=x86_64-conda-linux-gnu-gcc -DCMAKE_CXX_COMPILER=x86_64-conda-linux-gnu-g++ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 else
-    cmake ..
+    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 fi
 make
